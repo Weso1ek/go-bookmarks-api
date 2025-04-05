8200fdf81efcfadc804ea4bd8f2be7deab9d02d2
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2025-04-05 21:54:23 +0200

    First test endpoint

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"bookmark-api/httpcontroller"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	categoriesController := httpcontroller.NewCategoriesController()
+
+	router := gin.Default()
+	router.GET("/albums", categoriesController.GetCategories)
+
+	router.Run("localhost:8080")
 }
