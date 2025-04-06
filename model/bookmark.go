9cbe0744b93176f92f3abbfd318c0809ee505a7e
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2025-04-06 22:15:08 +0200

    Create models

---
+++ model/bookmark.go
@@ -0,0 +1,8 @@
+package model
+
+type Bookmark struct {
+	Id          int32
+	Title       string
+	Url         string
+	Description string
+}
---
+++ model/category.go
@@ -0,0 +1,12 @@
+package model
+
+type Category struct {
+	Id        int32
+	Title     string
+	Body      string
+	MetaTitle string
+	MetaDesc  string
+	Logo      string
+	Path      string
+	SLug      string
+}
